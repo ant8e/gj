db78fb7daa0c3855c8541ab3f2178d26d961e803
Antoine Comte <antoine.comte@gmail.com>
2014-02-27 11:50:55 +0100

    Added License & Copyright

--- src/test/scala/UiServerSpec.scala
+++ src/test/scala/UiServerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2014 Antoine Comte
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.scalatest.FunSpec
 import org.scalatest.matchers.MustMatchers
 import spray.http._
@@ -11,7 +27,7 @@ import ui.UIServerRoute
  *
  */
 class UiServerSpec extends FunSpec with ScalatestRouteTest with UIServerRoute with MustMatchers {
-  def actorRefFactory = system // connect the DSL to the test ActorSystem
+  def actorRefFactory = system
 
 
   val `text/html(UTF8)` = ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`)
